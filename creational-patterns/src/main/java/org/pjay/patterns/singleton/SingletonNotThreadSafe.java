bafb43b6a95246043c2ccf61b17d6bc72dd4b7bc
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-23 10:44:34 +0530

    singleton

--- creational-patterns/src/main/java/org/pjay/patterns/singleton/SingletonNotThreadSafe.java
+++ creational-patterns/src/main/java/org/pjay/patterns/singleton/SingletonNotThreadSafe.java
@@ -4,9 +4,15 @@ package org.pjay.patterns.singleton;
  * @author vijayk
  */
 public class SingletonNotThreadSafe {
-    private static SingletonNotThreadSafe singletonNotThreadSafe = null;
+    // private static SingletonNotThreadSafe singletonNotThreadSafe = null;
+    private static volatile SingletonNotThreadSafe singletonNotThreadSafe = null;
 
+    // private SingletonNotThreadSafe() {}
     private SingletonNotThreadSafe() {
+        // Avoid creating object using reflection by throwing runtime exception
+        if (null != singletonNotThreadSafe) {
+            throw new RuntimeException("Use getInstance() method to create object of SingletonNotThreadSafe");
+        }
     }
 
     public static SingletonNotThreadSafe getInstance() {
